cf0eabe249d3541d21ed29f595c25e811db94485
Justin62913 <justin62913@gmail.com>
2020-03-15 15:59:56 -0600

    Docs:  changed comment in bikeshare_2.py file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -156,7 +156,7 @@ def user_stats(df):
 
 
 def display_data(df):
-    """ This displays contents of the csv files if the users requests them."""
+    """ This displays contents of the csv files if the user requests them."""
     start_loc = 0
     end_loc = 5
 
